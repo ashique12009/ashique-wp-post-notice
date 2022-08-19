c924302ed84bf4c64a1fc9103df920477ad4aa25
Ashique <ashique12009@gmail.com>
2022-08-19 17:09:12 +0600

    notice saved

--- inc/Post_Notice_Editor.php
+++ inc/Post_Notice_Editor.php
@@ -4,6 +4,7 @@ class Post_Notice_Editor
     public function __construct()
     {
         add_action('add_meta_boxes', [$this, 'add_meta_box']);
+        add_action('save_post', [$this, 'save_post_notice']);
     }
 
     public function add_meta_box()
@@ -21,5 +22,27 @@ class Post_Notice_Editor
     public function post_notice_display()
     {
         require_once ASHIQUE_WP_POST_NOTICE_PATH . '/views/post-notice-editor.php';
+    }
+
+    public function save_post_notice($post_id)
+    {
+        if ($this->user_can_save($post_id)) {
+            return;
+        }
+
+        if ((isset($_POST['ashique-post-notice-nonce']) && wp_verify_nonce('ashique-post-notice-nonce', 'ashique-post-notice-save'))) {
+            return;
+        }
+
+        $post_notice = strip_tags($_POST['ashique-admin-post-notice-editor']);
+        update_post_meta($post_id, 'ashique-post-notice', $post_notice);
+    }
+
+    public function user_can_save($post_id)
+    {
+        $is_autosave = wp_is_post_autosave($post_id);
+        $is_revision = wp_is_post_revision($post_id);
+
+        return ($is_autosave || $is_revision);
     }
 }
--- views/post-notice-editor.php
+++ views/post-notice-editor.php
@@ -1,2 +1,5 @@
 <p>Enter you notice:</p>
-<textarea id="ashique-admin-post-notice-editor"></textarea>
+<textarea id="ashique-admin-post-notice-editor" name="ashique-admin-post-notice-editor">
+    <?php echo get_post_meta(get_the_ID(), 'ashique-post-notice', true); ?>
+</textarea>
+<?php wp_nonce_field('ashique-post-notice-save', 'ashique-post-notice-nonce'); ?>
