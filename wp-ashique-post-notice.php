ac3945d07476755c877e0ecd62efe84090dde5a8
Ashique <ashique12009@gmail.com>
2022-08-19 10:47:59 +0600

    initial commit

---
+++ wp-ashique-post-notice.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Plugin Name:       A basic post notice plugin
+ * Plugin URI:        https://ashique12009.blogspot.com/
+ * Description:       A basic post notice plugin.
+ * Version:           1.0.0
+ * Author:            Khandoker Ashique Mahamud
+ * Author URI:        https://ashique12009.blogspot.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-ashique-post-notice
+ * Domain Path:       /languages
+ */
+
+// If this file called directly, abort
+if (!defined('WPINC')) {
+    die;
+}
+
